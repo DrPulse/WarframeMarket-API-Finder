70ef2dc0be1d3795a069b82f3f693d2f032ff8d0
DrPulse <1mpulseprng@gmail.com>
2021-08-06 21:50:19 +0200

    readme infos

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # WarframeMarket-API-Finder
-Python script to find Warframe market infos and settings alerts
+
+Python script to find Warframe market infos about items price and settings alerts for set prices
+
+# Documentation
+
+This script is using the currently only API available for Warframe Market : apiVersion : v1
+documentation of the API : https://warframe.market/fr/api_docs
+
+# Usage
+
+# Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
--- WM API.py
+++ WM API.py
@@ -7,29 +7,29 @@ import webbrowser
 #URL and credentials storage
 password = ""
 mail = ""
-Token_JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzaWQiOiI1T0RhSFRVZUN4Zm9iVkUxcjlMVDk5Y1UwZmU3N09rdCIsImNzcmZfdG9rZW4iOiI5OTJmMzM3NWY0Yzk2ODY2N2Y2YWIwMjU4NTM5OGYxZjVjZmNmNTQ0IiwiZXhwIjoxNjMzMjAzODQzLCJpYXQiOjE2MjgwMTk4NDMsImlzcyI6Imp3dCIsImF1ZCI6Imp3dCIsImF1dGhfdHlwZSI6ImNvb2tpZSIsInNlY3VyZSI6ZmFsc2UsImp3dF9pZGVudGl0eSI6Ijk5RDNNNUZvTDFtUnJIck9iVkVYcnAxbTE3NHZlUDJDIiwibG9naW5fdWEiOiJiJ01vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjkwLjApIEdlY2tvLzIwMTAwMTAxIEZpcmVmb3gvOTAuMCciLCJsb2dpbl9pcCI6ImInOTMuMjEuOTkuODgnIn0.mslfC-t2MlPi3gmJjgWUDHbAfBzV5u1i3WITLOP1x9U"
+Token_JWT = ""
 main_URL = "https://api.warframe.market/v1/items"
 login_URL = "https://api.warframe.market/v1/auth/signin"
 profile_URL = "https://api.warframe.market/v1/profile"
 
 #Testing API responses
 print("\nTesting API responses")
-responseR = requests.get(main_URL)
+WMresponseR = requests.get(main_URL)
 print("")
-print(responseR)
-print("API OK\n\n")
+print(WMresponseR)
+print("API OK\n")
 
 """
 #login
-loginR = requests.post(login_URL, headers = {'Authorization' : 'Token_JWT', 'language' : 'en', 'accept' : 'application/json', 'platform' : 'pc', 'auth_type' : 'header'}, json={'email' : mail, 'password' : password, 'auth_type' : 'header'})
-print(loginR)
-#pprint.pprint(l.json()) #Payload checking for credentials (debugging)
+WMloginR = requests.post(login_URL, headers = {'Authorization' : 'Token_JWT', 'language' : 'en', 'accept' : 'application/json', 'platform' : 'pc', 'auth_type' : 'header'}, json={'email' : mail, 'password' : password, 'auth_type' : 'header'})
+print(WMloginR)
+#pprint.pprint(WMlogin.json()) #Payload checking for credentials (debugging)
 print("login OK\n")
 """
 """
-p = requests.get(profile_URL)
-p.json()
-pprint.pprint(p.json())
+WMprofileR = requests.get(profile_URL)
+WMprofileR.json()
+pprint.pprint(WMprofileR.json())
 """
 
 
@@ -39,17 +39,17 @@ search = input()
 print("\nSearching for " + search + "...\n")
 
 #search = "mesa_prime_set"
-itemR = requests.get('https://api.warframe.market/v1/items/' + search.replace(' ', '_') + '/statistics')
-#itemR = requests.get('https://api.warframe.market/v1/items/mesa_prime_set/statistics') # a retirer a terme
-#pprint.pprint(itemR.json())
-print(itemR)
-if(itemR.status_code == 200):
+WMitemR = requests.get('https://api.warframe.market/v1/items/' + search.replace(' ', '_') + '/statistics')
+#WMitemR = requests.get('https://api.warframe.market/v1/items/mesa_prime_set/statistics') # a retirer a terme
+#pprint.pprint(WMitemR.json())
+print(WMitemR)
+if(WMitemR.status_code == 200):
     print("Request OK\n")
-elif(itemR.status_code == 404):
+elif(WMitemR.status_code == 404):
     print("Request denied\n")
 
 #Output data in a json file
-data = itemR.json()
+data = WMitemR.json()
 with open ('data.json', 'w') as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
 
